19bb9c61ef8c889351d121e9f054605deb58fdef
Isaac Cha <ijcha93@gmail.com>
2023-03-28 19:28:24 -0400

    edit

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Contact() {
     return (
-        <div className=" d-flex justify-content-center align-items-center ">
+        <div className="d-flex justify-content-center align-items-center ">
             
         <section className="">
 
